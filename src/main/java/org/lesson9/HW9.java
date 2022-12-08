0e14093ef015c30fe41a744b76dc658f1ae5ca89
Irina Kroshkina <ikroshkina@gmail.com>
2022-12-08 21:04:18 +0200

    - change set

--- src/main/java/org/lesson9/HW9.java
+++ src/main/java/org/lesson9/HW9.java
@@ -2,6 +2,8 @@ package org.lesson9;
 
 import com.sun.xml.internal.ws.util.StringUtils;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -9,12 +11,24 @@ public class HW9 {
 
     public static void main(String[] args) {
         String text = "масло, масло, колбаса, молоко";
-        transformer(text);
+    //    transformer(text);
+        transformer2(text);
     }
 
     private static void transformer(String text) {
         String[] strs = text.split(", ");
         Set<String> words = new TreeSet<>();
+        for(String str : strs) {
+            words.add(str);
+        }
+        for (String w : words) {
+            System.out.println(StringUtils.capitalize(w));
+        }
+    }
+
+    private static void transformer2(String text) {
+        String[] strs = text.split(", ");
+        Set<String> words = new LinkedHashSet<>();
         for(String str : strs) {
             words.add(str);
         }
